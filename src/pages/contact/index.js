fdc54117c19a1fb92e3553e88bcbdfce2dd70526
dad <60132399+dad2vad@users.noreply.github.com>
2020-03-03 08:34:47 +0200

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,20 +1,6 @@
-import { React, useState } from 'react'
+import  React, { useState } from 'react'
 import { navigate } from 'gatsby-link'
 import Layout from '../../components/Layout'
-
-
-  const [contact, setContact] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    phone: '', 
-    message: ''
-    })
-  
-  const [response, setResponse] = useState({
-    type: '',
-    message: ''
-  })
 
   const handleChange = e =>
     setContact({ ...contact, [e.target.name]: e.target.value })
@@ -33,7 +19,18 @@ export default class Index extends React.Component {
     super(props)
     this.state = { isValidated: false }
   }
-
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    phone: '', 
+    message: ''
+    })
+  
+  const [response, setResponse] = useState({
+    type: '',
+    message: ''
+  })
 //  const handleChange = e => {
 //     this.setState({ [e.target.name]: e.target.value })
 //   }

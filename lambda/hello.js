ff1ae6490103391d8921a866efec78e92c75668f
dad <60132399+dad2vad@users.noreply.github.com>
2020-03-03 02:33:28 +0200

    Update hello.js

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,7 +1,7 @@
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 //module.exports.handler = async function(event, context) {
 exports.handler = async event => {
-  console.log(JSON.stringify(event,null,4))
+  console.log(JSON.stringify(event.body,null,4))
   return {
     // return null to show no errors
     statusCode: 200, // http status code

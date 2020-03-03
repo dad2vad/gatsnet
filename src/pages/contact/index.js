60028a1c60fc037dff112d8bce0a0dad87b9c005
dad <60132399+dad2vad@users.noreply.github.com>
2020-03-03 07:39:29 +0200

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { navigate } from 'gatsby-link'
 import Layout from '../../components/Layout'
-import { navigate } from 'gatsby-link'
 
 function encode(data) {
   return Object.keys(data)

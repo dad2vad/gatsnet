7117e9108053668c383729faf586420eaa89acfe
dad <60132399+dad2vad@users.noreply.github.com>
2020-03-17 17:51:02 +0200

    Update file-upload.js

--- src/pages/contact/file-upload.js
+++ src/pages/contact/file-upload.js
@@ -51,8 +51,7 @@ export default class Contact extends React.Component {
                 name="file-upload"
                 method="post"
                 action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
+
                 onSubmit={this.handleSubmit}
               >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
